7c9d5f75a1703e141ebb70b10468058bf227798a
HippErger <berger.clayton@gmail.com>
2017-05-15 19:42:28 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import AppContainer from "./AppContainer";
 import "./index.css";
 import store from "./store";
 import {Provider} from "react-redux";
+import "whatwg-fetch";
+
+fetch("http://localhost:4001/products").then((response) => {
+  console.log("execute when loaded");
+  console.log(response);
+}).catch((err) => {
+  console.log("Oops, an error happened.");
+});
 
 ReactDOM.render(
   <Provider store={store}><AppContainer /></Provider>,
