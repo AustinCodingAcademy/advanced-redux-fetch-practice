75184defca97d56eac1caad39ab394639ecbee8a
Christian Pasti <christian.pasti@hotmail.com>
2017-05-17 15:56:33 -0500

    Added actions, reducers and modified containers

---
+++ src/actions/index.js
@@ -0,0 +1,146 @@
+// Call this function before sending the post request.
+// If it returns 0, the record is empty.
+// Otherwise, a blank record may be created.
+function checkEmpty(model) {
+    let counter = 0;
+    for(var k in model) {
+        if(model[k] != "") counter++;
+    }
+    return counter;
+}
+
+export function loadContacts() {
+    return function(dispatch) {
+        dispatch({
+            type: "LOAD_CONTACTS"
+        });
+
+        fetch("/contacts")
+        .then((response) => {
+            return response.json();
+        }).then((contacts) => {
+            dispatch(contactsLoaded(contacts));
+        });
+    }
+}
+
+export function contactsLoaded(contacts) {
+    return {
+        type: "CONTACTS_LOADED",
+        value: contacts
+    }
+}
+
+export function loadVehicles() {
+    return function(dispatch) {
+        dispatch({
+            type: "LOAD_VEHICLES"
+        });
+
+        fetch("/vehicles")
+        .then((response) => {
+            return response.json();
+        }).then((vehicles) => {
+            dispatch(vehiclesLoaded(vehicles));
+        });
+    }
+}
+
+export function vehiclesLoaded(vehicles) {
+    return {
+        type: "VEHICLES_LOADED",
+        value: vehicles
+    }
+}
+
+export function loadComments() {
+    return function(dispatch) {
+        dispatch({
+            type: "LOAD_COMMENTS"
+        });
+
+        fetch("/comments")
+        .then((response) => {
+            return response.json();
+        }).then((comments) => {
+            dispatch(commentsLoaded(comments));
+        });
+    }
+}
+
+export function commentsLoaded(comments) {
+    return {
+        type: "COMMENTS_LOADED",
+        value: comments
+    }
+}
+
+export function loadProducts() {
+    return function(dispatch) {
+        dispatch({
+            type: "LOAD_PRODUCTS"
+        });
+
+        fetch("/products")
+        .then((response) => {
+            return response.json();
+        }).then((products) => {
+            dispatch(productsLoaded(products));
+        });
+    }
+}
+
+export function productsLoaded(products) {
+    return {
+        type: "PRODUCTS_LOADED",
+        value: products
+    }
+}
+
+export function createProduct(product) {
+    if(checkEmpty(product) > 0) {
+        return function(dispatch) {
+            fetch("/products", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(product)
+            }).then(() => dispatch(loadProducts()));
+        }
+    }
+}
+
+export function createContact(contact) {
+    if(checkEmpty(contact) > 0) {
+        return function(dispatch) {
+            fetch("/contacts", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(contact)
+            }).then(() => dispatch(loadContacts()));
+        }
+    }
+}
+
+export function createComment(comment) {
+    if(checkEmpty(comment) > 0) {
+        return function(dispatch) {
+            fetch("/comments", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(comment)
+            }).then(() => dispatch(loadComments()));
+        }
+    }
+}
+
+export function createVehicle(vehicle) {
+    if(checkEmpty(vehicle) > 0) {
+        return function(dispatch) {
+            fetch("/vehicles", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(vehicle)
+            }).then(() => dispatch(loadVehicles()));
+        }
+    }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,38 @@
 import {combineReducers} from "redux";
 
 function comments(state = [], action) {
+  if(action.type === "COMMENTS_LOADED") {
+    state = action.value;
+  }
+  return state;
+}
+
+function contacts(state = [], action) {
+  if(action.type === "CONTACTS_LOADED") {
+    state = action.value;
+  }
+  return state;
+}
+
+function products(state = [], action) {
+  if(action.type === "PRODUCTS_LOADED") {
+    state = action.value;
+  }
+  return state;
+}
+
+function vehicles(state = [], action) {
+  if(action.type === "VEHICLES_LOADED") {
+    return action.value;
+  }
   return state;
 }
 
 const rootReducer = combineReducers({
-  comments
+  comments,
+  contacts,
+  products,
+  vehicles
 });
+
 export default rootReducer;
