132ba2287de9a33895b910b86508c701eb2696fe
joeycaradonna <joeycaradonna45@gmail.com>
2017-05-18 16:47:06 -0500

    fetch practice working

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,9 +1,22 @@
 import App from "./App";
 import "./App.css";
 import { connect } from "react-redux";
+import {loadComments, loadContacts, loadProducts, loadVehicles} from "./actions";
 
 function mapDispatchToProps(dispatch) {
   return {
+    loadComments(){
+      dispatch(loadComments());
+    },
+    loadContacts(){
+      dispatch(loadContacts());
+    },
+    loadProducts(){
+      dispatch(loadProducts());
+    },
+    loadVehicles(){
+      dispatch(loadVehicles());
+    }
 
   };
 }
