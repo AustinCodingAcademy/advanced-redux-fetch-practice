5715a73841ec20263805932b9a3d7fc0bcba6f0d
MicahMonfrini <ffrock905@gmail.com>
2017-05-18 21:52:08 -0500

    comments reducer created

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,15 @@ import {combineReducers} from "redux";
 import {
   PRODUCT_LOAD_SUCCESS,
   CONTACTS_LOAD_SUCCESS,
-  VEHICLES_LOAD_SUCCESS
+  VEHICLES_LOAD_SUCCESS,
+  COMMENTS_LOAD_SUCCESS
 } from "../actions";
 
 function comments(state = [], action) {
+  switch (action.type) {
+    case COMMENTS_LOAD_SUCCESS:
+      return action.comments;
+  }
   return state;
 }
 
