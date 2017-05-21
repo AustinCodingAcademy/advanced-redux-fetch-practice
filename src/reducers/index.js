2df9051b8d1dd53ef9b95bb90933d1fb579809ad
Nick Rodriguez <Nick Rodriguez>
2017-05-21 12:27:02 -0500

    fetch practice 1

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,29 @@
+/*
+Reducer file
+
+Gets datat from an action.  This is done with Dispatch
+in the action file
+*/
+
 import {combineReducers} from "redux";
+import {PRODUCT_LOAD_SUCCESS} from "../actions/";
+
 
 function comments(state = [], action) {
   return state;
 }
 
+function products(state = [], action) {
+  switch (action.type) {
+    case PRODUCT_LOAD_SUCCESS:
+      return action.products;
+  }
+  return state;
+}
+
+
 const rootReducer = combineReducers({
-  comments
+  comments,
+  products
 });
 export default rootReducer;
