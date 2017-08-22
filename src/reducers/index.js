c0194031ff2668d2d49619aba48ccb23944d162d
Greg Devany <greg.devany@yahoo.com>
2017-08-22 18:47:37 -0500

    Started making actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,34 @@
 import {combineReducers} from "redux";
 
 function comments(state = [], action) {
+  if(action.type === "COMMENTS_LOADED") {
+    return action.type;
+  }
+  return state;
+}
+
+function vehicles(state = [], action) {
+  if(action.type === "VEHICLES_LOADED") {
+    return action.type;
+  }
+  return state;
+}
+
+function contacts(state = [], action) {
+  if(action.type === "CONTACTS_LOADED") {
+    return action.type;
+  }
+  return state;
+}
+
+function products(state = [], action) {
+  if(action.type === "PRODUCTS_LOADED") {
+    return action.type;
+  }
   return state;
 }
 
 const rootReducer = combineReducers({
-  comments
+  comments, vehicles, contacts, products
 });
 export default rootReducer;
