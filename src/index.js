62482e603bf8e844e2e75405f2d4951b2b168bb5
Jesse-Paulk <paulkjesse@gmail.com>
2017-11-06 17:24:23 -0600

    Advanced Redux Fetch Jesse Paulk

--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import AppContainer from "./AppContainer";
-import "./index.css";
-import store from "./store";
-import {Provider} from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import App from './App';
+import './index.css';
+import state from './state';
+import store from './components/store.js';
+import { Provider } from 'react-redux';
+
+const {
+  dateTime,
+  newComments,
+  newTasks,
+  newOrders,
+  tickets,
+  orders,
+  taskItem,
+  tasks,
+  messages
+} = state;
+
+function renderApp() {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  );
+}
+renderApp();
 
 ReactDOM.render(
-  <Provider store={store}><AppContainer /></Provider>,
-  document.getElementById("root")
+  <App
+    taskItem={taskItem}
+    dateTime={dateTime}
+    newComments={newComments}
+    newTasks={newTasks}
+    newOrders={newOrders}
+    tickets={tickets}
+    orders={orders}
+    messages={messages}
+    tasks={tasks}
+  />,
+  document.getElementById('root')
 );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,30 @@
-import {combineReducers} from "redux";
-
-function comments(state = [], action) {
+import { combineReducers } from 'redux';
+function newComments(state = 0, action) {
   return state;
 }
 
+function newTasks(state = 0, action) {
+  return state;
+}
+
+function newOrders(state = 0, action) {
+  return state;
+}
+function tickets(state = 0, action) {
+  return state;
+}
+function tasks(state = [], action) {
+  return state;
+}
+function orders(state = 0, action) {
+  return state;
+}
 const rootReducer = combineReducers({
-  comments
+  newComments,
+  newTasks,
+  newOrders,
+  tickets,
+  orders,
+  tasks
 });
 export default rootReducer;
