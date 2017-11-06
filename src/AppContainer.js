d4abe5a1690b9edf2ce24e120ecaac147840a85d
Casey O'Keefe <the0surfer@aol.com>
2017-11-06 16:02:20 -0600

    finish all tasks

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,11 +1,26 @@
 import App from "./App";
 import "./App.css";
 import { connect } from "react-redux";
+import { loadContacts } from "./actions";
+import { loadVehicles } from "./actions";
+import { loadComments } from "./actions";
+import { loadProducts } from "./actions";
 
 function mapDispatchToProps(dispatch) {
   return {
-
-  };
+    loadContacts: function(contact) {
+      dispatch(loadContacts(contact));
+    },
+    loadVehicles: function(vehicle) {
+      dispatch(loadVehicles(vehicle));
+    },
+    loadComments: function(comment) {
+      dispatch(loadComments(comment));
+    },
+    loadProducts: function(product) {
+      dispatch(loadProducts(product));
+    }
+  }
 }
 
 export default connect(null,mapDispatchToProps)(App);
