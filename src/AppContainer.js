c56f6808c0f1dd4bacdb0b816cb9f2e450afab9e
CGottschallJ <Cameron.Gottschall@gmail.com>
2017-11-06 20:46:57 -0600

    Final:completed app

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,11 +1,26 @@
 import App from "./App";
 import "./App.css";
 import { connect } from "react-redux";
-import {loadActions, loadVehicles, loadComments, loadProducts} from './actions';
+import {loadContacts, loadVehicles, loadComments, loadProducts} from './actions';
 
 function mapDispatchToProps(dispatch) {
   return {
-
+    loadContacts: function(){
+      let action = loadContacts();
+      dispatch(action);
+    },
+    loadVehicles: function(){
+      let action = loadVehicles();
+      dispatch(action);
+    },
+    loadComments: function(){
+      let action = loadComments();
+      dispatch(action);
+    },
+    loadProducts: function(){
+      let action = loadProducts();
+      dispatch(action);
+    }
   };
 }
 
