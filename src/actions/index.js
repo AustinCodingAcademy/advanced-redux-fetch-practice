c3c30bea4a8f0d30a993a4a5c77a56cb0884fda2
Jordon R <reese.jordon@gmail.com>
2017-11-26 20:05:42 -0600

    Create data actions add

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,15 @@
 
+// actions that will retrieve all the data using fetch calls
+// then turn the call response into a json object
+// then dispatch to another function that the call has been complete
+// then set the value of each call into the database
 
 export function loadContacts() {
   return function(dispatch){
     dispatch({
-      type:"LOAD_CONTACTS"
+      type: "LOAD_CONTACTS"
     });
-    fetch("http://localhost:4001/contacts").then((response) => {
+    fetch("/contacts").then((response) => {
       return response.json();
     }).then((contacts) => {
       dispatch(contactsLoaded(contacts));
@@ -23,9 +27,9 @@ export function contactsLoaded(contacts){
 export function loadVehicles() {
   return function(dispatch) {
     dispatch({
-      type:"LOAD_VEHICLES"
+      type: "LOAD_VEHICLES"
     });
-    fetch("http://localhost:4001/vehicles").then((response) => {
+    fetch("/vehicles").then((response) => {
       return response.json();
     }).then((vehicles) => {
       dispatch(vehiclesLoaded(vehicles));
@@ -43,9 +47,9 @@ export function vehiclesLoaded(vehicles){
 export function loadComments() {
   return function(dispatch) {
     dispatch({
-      type:"LOAD_COMMENTS"
+      type: "LOAD_COMMENTS"
     });
-    fetch("http://localhost:4001/comments").then((response) => {
+    fetch("/comments").then((response) => {
       return response.json();
     }).then((comments) => {
       dispatch(commentsLoaded(comments));
@@ -55,7 +59,7 @@ export function loadComments() {
 
 export function commentsLoaded(comments) {
   return {
-    type:"COMMENTS_LOADED",
+    type: "COMMENTS_LOADED",
     value: comments
   };
 }
@@ -63,9 +67,9 @@ export function commentsLoaded(comments) {
 export function loadProducts() {
   return function(dispatch){
     dispatch({
-      type:"LOAD_PRODUCTS"
+      type: "LOAD_PRODUCTS"
     });
-    fetch("http://localhost:4001/products").then((response) => {
+    fetch("/products").then((response) => {
       return response.json();
     }).then((products) => {
       dispatch(productsLoaded(products));
@@ -78,4 +82,66 @@ export function productsLoaded(products){
     type: "PRODUCTS_LOADED",
     value: products
   };
+}
+
+
+// create actions to generate all the data
+export function createProduct(product){
+  return function(dispatch) {
+    fetch("/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product)
+    })
+    .then((response) => {
+      dispatch(loadProducts());
+    });
+  }
+}
+
+export function createContact(contact) {
+  return function(dispatch) {
+    fetch("/contacts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(contact)
+    })
+    .then((response) => {
+      dispatch(loadContacts());
+    });
+  }
+}
+
+export function createComment(comment) {
+  return function(dispatch) {
+    fetch("/comments", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(comment)
+    })
+    .then((response) => {
+      dispatch(loadComments());
+    });
+  }
+}
+
+export function createVehicle(vehicle) {
+  return function(dispatch) {
+    fetch("/vheicles", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(vehicle)
+    })
+    .then((response) => {
+      dispatch(loadVehicles());
+    });
+  }
 }
