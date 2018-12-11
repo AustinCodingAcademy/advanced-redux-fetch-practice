578a7ad5c8d4c90346662e2101fa834a2153226c
SeanPhilippi <sean.philippi@gmail.com>
2018-12-10 21:43:10 -0600

    reducers finished

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,37 @@
 import { combineReducers } from "redux";
 
-function comments(state = [], action) {
-  return state;
+function products(state = [], action) {
+  return (
+    action.type === "PRODUCTS_LOADED" ?
+      action.value :
+      state
+  );
 }
 
-function products(state = [], action) {
-  if (action.type == "PRODUCTS_LOADED") {
-    return action.value;
-  }
-  return state;
+function comments(state = [], action) {
+  return (
+    action.type === "COMMENTS_LOADED" ?
+      action.value :
+      state
+  );
+}
+
+function contacts(state = [], action) {
+  return (
+    action.type === "CONTACTS_LOADED" ?
+      action.value :
+      state
+  );
+}
+
+function vehicles(state = [], action) {
+  return (
+    action.type === "VEHICLES_LOADED" ?
+      action.value :
+      state
+  );
 }
 
 export default combineReducers({
-  comments, products
+  comments, products, vehicles, contacts
 });
