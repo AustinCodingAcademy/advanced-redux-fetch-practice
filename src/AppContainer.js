47c9d0b99b447e7ae84eea5e25b52a19fed44864
cynthiawilliamsa <cynthiawilliamsa@gmail.com>
2018-12-17 11:54:38 -0600

    setup actions and reducers for contacts, vehicles and comments

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,6 +1,10 @@
 import App from "./App";
 import "./App.css";
 import { connect } from "react-redux";
+import {loadProducts} from './actions';
+import {loadComments} from './actions';
+import {loadContacts} from './actions';
+import {loadVehicles} from './actions';
 
 function mapDispatchToProps(dispatch) {
   return {
@@ -8,4 +12,12 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(null,(d)=>{
+  return {
+    loadProducts: ()=>d(loadProducts());
+
+
+  }
+})(App);
+
+// export default connect(null,mapDispatchToProps)(App);
